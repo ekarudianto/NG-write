a0b93858d7bdcd52f24a08805fe7944f78431deb
deal1 <eka@dealservices.nl>
2016-04-18 14:35:32 +0700

    add comments

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,29 @@ module.exports = {
         ]
     },
     plugins: [
+
+    /**
+     * minify js
+     */
+
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         }),
+
+    /**
+     * resolver plugin, for using bower components
+     */
+
         new webpack.ResolverPlugin(
             new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
         ),
+
+    /**
+     * provide plugin, load the component the first time
+     */
+
         new webpack.ProvidePlugin({
             //'angular': 'exports?window.angular!angular',
             $: "jquery"
