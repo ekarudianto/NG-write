70abe589cfc5fb822d831bf594832ffc982690b1
deal1 <eka@dealservices.nl>
2016-04-18 12:00:07 +0700

    adding comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,34 @@ var app = angular.module('app', [
     'ngCookies'
 ]);
 
+/**
+ * config collections
+ */
+
 app.config(require('./route'));
+
+/**
+ * constant / value collections
+ */
+
 app.constant('CONFIG', require('./config'));
+
+/**
+ * controller collections
+ */
+
 app.controller('home', require('./controllers/home'));
+
+/**
+ * directive collections
+ */
+
 app.directive('version', require('./directives/version'));
+
+/**
+ * run method
+ */
+
 app.run(require('./run'));
 
 module.exports = app;
