0c36ad9ed8d84d4db2245c998ef23c62f6e4e688
deal1 <eka@dealservices.nl>
2016-04-18 14:33:34 +0700

    add comments

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
+/**
+ * load component dependencies
+ */
+
 require('angular');
 require('angular-route');
 require('angular-cookies');
+
+/**
+ * load main app
+ */
 
 require('./app');
